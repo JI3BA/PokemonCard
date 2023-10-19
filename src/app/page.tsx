030ed49d6ccac0ea45382197bfa1e1f6937736ec
JI3BA <lyovkin_artyom@mail.ru>
2023-10-19 20:01:00 +0300

    main page was fixed, axios will be replaced by RTK Query

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,24 +1,7 @@
 'use client'
-import axios, {AxiosInstance} from "axios";
-import {useEffect, useState} from "react";
-import type {PokemonListType} from "@/models/PokemonList";
-import {v1} from "uuid";
-import Link from "next/link";
-
-const request = axios.create({
-    baseURL: `https://pokeapi.co/api/v2`,
-})
-
-const getRequestPokemon = async(request: AxiosInstance, endpoint: string) => {
-    return await request.get(endpoint)
-}
 
 export default function Home() {
-    const [pokemon, setPokemon] = useState<PokemonListType[]>()
 
-    useEffect(() => {
-        getRequestPokemon(request, 'pokemon').then(res => setPokemon(res.data.results))
-    },[])
 
 
     return (
@@ -26,19 +9,7 @@ export default function Home() {
             <h1>hello JI3BA</h1>
 
             <div className='pokemon'>
-                <ul className='pokemon__list'>
-                    {!pokemon
-                        ? <p className="loader"></p>
-                        :pokemon.map((pok, i) => <li key={v1()}>
-                        <Link href={`/pokemon/${i+1}`}>{pok.name}</Link>
-                    </li>)}
-                </ul>
-                {/*{pokemon &&*/}
-                {/*    <>*/}
-                {/*        <p>Name: <span style={{fontWeight: 700}}>{pokemon.name}</span></p>*/}
-                {/*        <img src={pokemon.sprites.other.dream_world.front_default} alt={pokemon.name}/>*/}
-                {/*    </>*/}
-                {/*}*/}
+
             </div>
         </div>
     )
