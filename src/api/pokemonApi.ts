d07665cca9c0167e441758c01f7068313025ca7d
JI3BA <lyovkin_artyom@mail.ru>
2023-10-20 18:16:34 +0300

    pokemonApi was created

---
+++ src/api/pokemonApi.ts
@@ -0,0 +1,14 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
+import {IPokemon} from "@/models/Pokemon";
+
+export const pokemonApi = createApi({
+    reducerPath: 'pokemonApi',
+    baseQuery: fetchBaseQuery({ baseUrl: 'https://pokeapi.co/api/v2/' }),
+    endpoints: (builder) => ({
+        getPokemonByName: builder.query<IPokemon, string>({
+            query: (name) => `pokemon/${name}`,
+        }),
+    }),
+})
+
+export const { useGetPokemonByNameQuery } = pokemonApi
