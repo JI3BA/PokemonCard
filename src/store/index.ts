36a50b6900f7ea0dfd476719772507e421fd3d91
JI3BA <lyovkin_artyom@mail.ru>
2023-10-21 19:31:07 +0300

    configure store was fixed

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,16 +1,17 @@
 import { configureStore } from '@reduxjs/toolkit'
 import {pokemonSlice} from "@/store/slices/pokemonSlice";
 import {pokemonApi} from "@/api/pokemonApi";
+import {setupListeners} from "@reduxjs/toolkit/query";
 
 export const store = configureStore({
     reducer: {
-        pokemon: pokemonSlice,
         [pokemonApi.reducerPath]: pokemonApi.reducer,
-
-        middleware: (getDefaultMiddleware) =>
-            getDefaultMiddleware().concat(pokemonApi.middleware)
+        pokemon: pokemonSlice,
     },
+    middleware: (getDefaultMiddleware) =>
+        getDefaultMiddleware().concat(pokemonApi.middleware)
 })
 
+setupListeners(store.dispatch)
 export type RootState = ReturnType<typeof store.getState>
 export type AppDispatch = typeof store.dispatch
