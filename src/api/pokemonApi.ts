77148f6d06fe17da37e646b40142d00fcb3f5dbf
JI3BA <lyovkin_artyom@mail.ru>
2023-10-22 19:55:55 +0300

    endpoint is drawing a list of Pokemon was added to pokemonApi

--- src/api/pokemonApi.ts
+++ src/api/pokemonApi.ts
@@ -1,14 +1,18 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 import {IPokemon} from "@/models/Pokemon";
+import {PokemonListType} from "@/models/PokemonList";
 
 export const pokemonApi = createApi({
     reducerPath: 'pokemonApi',
     baseQuery: fetchBaseQuery({ baseUrl: 'https://pokeapi.co/api/v2/' }),
     endpoints: (builder) => ({
+        getPokemonList:builder.query<PokemonListType>({
+            query: () => `pokemon/`,
+        }) ,
         getPokemonByName: builder.query<IPokemon, string>({
-            query: (id) => `pokemon/${id}`,
+            query: (name) => `pokemon/${name}`,
         }),
     }),
 })
 
-export const { useGetPokemonByNameQuery } = pokemonApi
+export const { useGetPokemonByNameQuery, useGetPokemonListQuery } = pokemonApi
