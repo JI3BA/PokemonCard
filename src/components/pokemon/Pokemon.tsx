aa46ca3c993e02fc800629337b4cf2ab75b079cf
JI3BA <lyovkin_artyom@mail.ru>
2023-10-22 19:57:39 +0300

    useGetPokemonListQuery was used to get and render pokemonList

--- src/components/pokemon/Pokemon.tsx
+++ src/components/pokemon/Pokemon.tsx
@@ -1,16 +1,24 @@
-import {useGetPokemonByNameQuery} from "@/api/pokemonApi";
+import {useGetPokemonByNameQuery, useGetPokemonListQuery} from "@/api/pokemonApi";
+import {useEffect} from "react";
 
 
 export const Pokemon = () => {
-    const {data, error, isLoading} = useGetPokemonByNameQuery('1')
+    //const {data, error, isLoading} = useGetPokemonByNameQuery('1')
+    const {data, error, isLoading} = useGetPokemonListQuery()
+
+    useEffect(() => {
+        console.log(data)
+    }, [data])
 
     return(
         <>
             {isLoading
                 ? <p className='loader'></p>
                 : <>
-                    <img src={data.sprites.other.dream_world.front_default} alt="data.name"/>
-                    <p>{data.name}</p>
+                    {data.results.map(pok => <p>{pok.name}</p>
+                    )}
+                    {/*<img src={data.sprites.other.dream_world.front_default} alt="data.name"/>*/}
+                    {/*<p>{data.name}</p>*/}
                 </>
             }
         </>
