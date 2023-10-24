54a3479903dec947a42e64557c50de748b7bfaa4
JI3BA <lyovkin_artyom@mail.ru>
2023-10-24 18:43:47 +0300

    extractRehydrationInfo was added to RTQ Query

--- src/api/pokemonApi.ts
+++ src/api/pokemonApi.ts
@@ -1,10 +1,16 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 import {IPokemon} from "@/models/Pokemon";
 import {PokemonListType} from "@/models/PokemonList";
+import { HYDRATE } from 'next-redux-wrapper'
 
 export const pokemonApi = createApi({
     reducerPath: 'pokemonApi',
     baseQuery: fetchBaseQuery({ baseUrl: 'https://pokeapi.co/api/v2/' }),
+    extractRehydrationInfo(action, { reducerPath }) {
+        if (action.type === HYDRATE) {
+            return action.payload[reducerPath]
+        }
+    },
     endpoints: (builder) => ({
         getPokemonList:builder.query<PokemonListType>({
             query: () => `pokemon/`,
